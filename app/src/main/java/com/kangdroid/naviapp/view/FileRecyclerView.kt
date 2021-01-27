f9820de6b07be662a3803ce0d5b9f19f0b2ea06f
zer0ken <a37592777@gmail.com>
2021-01-27 12:47:46 +0900

    file recycler view

---
+++ app/src/main/java/com/kangdroid/naviapp/view/FileRecyclerView.kt
@@ -0,0 +1,56 @@
+package com.kangdroid.naviapp.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.kangdroid.naviapp.R
+
+class FileData(
+    var id: Long = 0,
+    var fileName: String,
+    var fileType: String,
+    var nextToken: String,
+    var lastModifiedTime: String,
+) {
+    companion object {
+        const val FILE_TYPE_FILE = "File"
+        const val FILE_TYPE_FOLDER = "Folder"
+    }
+}
+
+class FileRecyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val imgFileType: ImageView = itemView.findViewById<ImageView>(R.id.img_file_type)
+    private val tvFileName: TextView = itemView.findViewById<TextView>(R.id.tv_file_name)
+    private val tvLastModifiedTime: TextView =
+        itemView.findViewById<TextView>(R.id.tv_last_modified_time)
+
+    fun bind(fileData: FileData) {
+        imgFileType.setImageResource(
+            when (fileData.fileType) {
+                FileData.FILE_TYPE_FILE -> R.drawable.ic_common_file_24
+                FileData.FILE_TYPE_FOLDER -> R.drawable.ic_common_folder_24
+                else -> R.drawable.ic_common_error_24
+            }
+        )
+        tvFileName.text = fileData.fileName
+        tvLastModifiedTime.text = fileData.lastModifiedTime
+    }
+}
+
+class FileRecyclerAdapter(_items: MutableList<FileData> = mutableListOf<FileData>()) :
+    RecyclerView.Adapter<FileRecyclerViewHolder>() {
+    var items: MutableList<FileData> = _items
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FileRecyclerViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_file, parent, false)
+        return FileRecyclerViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: FileRecyclerViewHolder, position: Int) =
+        holder.bind(items[position])
+}
