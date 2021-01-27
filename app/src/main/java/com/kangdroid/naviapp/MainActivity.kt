9662e8d870ecfe683215bea07d6e733391ee1f44
zer0ken <a37592777@gmail.com>
2021-01-27 12:48:49 +0900

    testing code/layout of file recycler view

--- app/src/main/java/com/kangdroid/naviapp/MainActivity.kt
+++ app/src/main/java/com/kangdroid/naviapp/MainActivity.kt
@@ -2,10 +2,51 @@ package com.kangdroid.naviapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.kangdroid.naviapp.view.FileData
+import com.kangdroid.naviapp.view.FileRecyclerAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fileRV: RecyclerView by lazy {
+            findViewById<RecyclerView>(R.id.rv_test).apply {
+                layoutManager = LinearLayoutManager(applicationContext)
+            }
+        }
+
+        val fileAdapter: FileRecyclerAdapter by lazy {
+            FileRecyclerAdapter().also { fileRV.adapter = it }
+        }
+
+        findViewById<Button>(R.id.btn_add_file).setOnClickListener {
+            fileAdapter.items.add(
+                FileData(
+                    fileAdapter.items.size.toLong(),
+                    "file-name-${fileAdapter.items.size.toLong() + 1}",
+                    FileData.FILE_TYPE_FILE,
+                    "TOKEN",
+                    "modified-time-${fileAdapter.items.size.toLong() + 1}"
+                )
+            )
+            fileAdapter.notifyDataSetChanged()
+        }
+
+        findViewById<Button>(R.id.btn_add_folder).setOnClickListener {
+            fileAdapter.items.add(
+                FileData(
+                    fileAdapter.items.size.toLong(),
+                    "folder-name-${fileAdapter.items.size.toLong() + 1}/",
+                    FileData.FILE_TYPE_FOLDER,
+                    "TOKEN",
+                    "modified-time-${fileAdapter.items.size.toLong() + 1}"
+                )
+            )
+            fileAdapter.notifyDataSetChanged()
+        }
     }
 }
