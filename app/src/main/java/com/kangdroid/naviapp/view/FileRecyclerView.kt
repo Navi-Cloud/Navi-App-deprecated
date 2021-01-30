7eb6ca6b348517b6130ca86dac63baaa8a164451
zer0ken <a37592777@gmail.com>
2021-01-30 12:01:02 +0900

    formatted code

--- app/src/main/java/com/kangdroid/naviapp/view/FileRecyclerView.kt
+++ app/src/main/java/com/kangdroid/naviapp/view/FileRecyclerView.kt
@@ -24,7 +24,7 @@ class FileRecyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     private val imgFileType: ImageView = itemView.findViewById<ImageView>(R.id.img_file_type)
     private val tvFileName: TextView = itemView.findViewById<TextView>(R.id.tv_file_name)
     private val tvLastModifiedTime: TextView =
-        itemView.findViewById<TextView>(R.id.tv_last_modified_time)
+            itemView.findViewById<TextView>(R.id.tv_last_modified_time)
 
     fun bind(fileData: FileData) {
         imgFileType.setImageResource(
@@ -39,8 +39,8 @@ class FileRecyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
 
 class FileRecyclerAdapter(_items: MutableList<FileData> = mutableListOf<FileData>()) :
-    RecyclerView.Adapter<FileRecyclerViewHolder>() {
-    var items: MutableList<FileData> = _items
+        RecyclerView.Adapter<FileRecyclerViewHolder>(), MutableCollection<FileData> by _items {
+    private var items: MutableList<FileData> = _items
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FileRecyclerViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_file, parent, false)
@@ -50,5 +50,5 @@ class FileRecyclerAdapter(_items: MutableList<FileData> = mutableListOf<FileData
     override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: FileRecyclerViewHolder, position: Int) =
-        holder.bind(items[position])
+            holder.bind(items[position])
 }
