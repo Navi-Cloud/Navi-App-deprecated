086c56fdd2c971c5d3a9c697d8e2bf45496c3f4b
zer0ken <a37592777@gmail.com>
2021-02-02 21:28:21 +0900

    created class SortedFileList(sort on add), FileSortingMode

--- app/src/main/java/com/kangdroid/naviapp/view/FileRecyclerView.kt
+++ app/src/main/java/com/kangdroid/naviapp/view/FileRecyclerView.kt
@@ -58,3 +58,67 @@ class FileRecyclerAdapter(_items: MutableList<FileData> = mutableListOf<FileData
     override fun onBindViewHolder(holder: FileRecyclerViewHolder, position: Int) =
             holder.bind(items[position])
 }
+
+class SortedFileList : ArrayList<FileData>() {
+    var comparator: FileSortingMode = FileSortingMode.TypedName
+        set(value) {
+            field = value
+            sort()
+        }
+
+    var reversed: Boolean = false
+        set(value) {
+            field = value
+            sort()
+        }
+
+    private fun sort() {
+        if (reversed) {
+            sortWith(comparator)
+            reverse()
+        } else {
+            sortWith(comparator)
+        }
+        println("sorted")
+    }
+
+    override fun add(element: FileData): Boolean {
+        var done: Boolean = false
+        for (i in indices) {
+            val compare = comparator.compare(this[i], element)
+            if (reversed) {
+                if (compare <= 0) {
+                    add(i, element)
+                    done = true
+                    break
+                } else {
+                    continue
+                }
+            } else if (compare >= 0) {
+                add(i, element)
+                done = true
+                break
+            } else {
+                continue
+            }
+        }
+        if (!done) {
+            done = super.add(element)
+        }
+        return done
+    }
+}
+
+sealed class FileSortingMode : Comparator<FileData> {
+    object TypedName : FileSortingMode(),
+        Comparator<FileData> by compareBy({ it.fileType }, { it.fileName }, { it.lastModifiedTime })
+
+    object TypedLMT : FileSortingMode(),
+        Comparator<FileData> by compareBy({ it.fileType }, { it.lastModifiedTime }, { it.fileName })
+
+    object Name : FileSortingMode(),
+        Comparator<FileData> by compareBy({ it.fileName }, { it.lastModifiedTime })
+
+    object LMT : FileSortingMode(),
+        Comparator<FileData> by compareBy({ it.lastModifiedTime }, { it.fileName })
+}
