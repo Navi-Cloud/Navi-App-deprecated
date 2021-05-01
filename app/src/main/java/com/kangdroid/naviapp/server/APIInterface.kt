87069d3de2568a7595505c33097520bd7a487e97
0JIEUN0 <jieun4436@naver.com>
2021-05-01 12:39:46 +0900

    Change API endpoints

--- app/src/main/java/com/kangdroid/naviapp/server/APIInterface.kt
+++ app/src/main/java/com/kangdroid/naviapp/server/APIInterface.kt
@@ -9,17 +9,18 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface APIInterface {
-    @GET("/api/navi/rootToken")
-    fun getRootToken(): Call<ResponseBody>
-    @GET("/api/navi/findInsideFiles/{token}")
-    fun getInsideFiles(@Path("token") token: String): Call<List<FileData>>
+    @GET("/api/navi/root-token")
+    fun getRootToken(@HeaderMap headerMap: HashMap<String, Any>): Call<ResponseBody>
+
+    @GET("/api/navi/files/list/{token}")
+    fun getInsideFiles(@HeaderMap headerMap: HashMap<String, Any>, @Path("token") token: String): Call<List<FileData>>
 
     @Multipart
-    @POST("/api/navi/fileUpload")
-    fun upload(@PartMap par : HashMap<String,Any>, @Part files: MultipartBody.Part) : Call<ResponseBody>
+    @POST("/api/navi/files")
+    fun upload(@HeaderMap headerMap: HashMap<String, Any>, @PartMap par : HashMap<String,Any>, @Part files: MultipartBody.Part) : Call<ResponseBody>
 
-    @GET("api/navi/fileDownload/{token}")
-    fun download(@Path("token") token : String) : Call<ResponseBody>
+    @GET("api/navi/files/{token}")
+    fun download(@HeaderMap headerMap: HashMap<String, Any>, @Path("token") token : String) : Call<ResponseBody>
 
     @POST("/api/navi/login")
     fun loginUser( @Body userLoginRequest : LoginRequest ): Call<ResponseBody>
